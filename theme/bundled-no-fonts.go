7eb484a4425c1de0d1818833f0786ea3e71259fa
jason.xie@victheme.com <jason.xie@victheme.com>
2025-11-12 07:46:51 +0800

    Improving the no_fonts

---
+++ theme/bundled-no-fonts.go
@@ -0,0 +1,17 @@
+//go:build no_fonts
+
+package theme
+
+import "fyne.io/fyne/v2"
+
+var regular fyne.Resource
+
+var bold fyne.Resource
+
+var bolditalic fyne.Resource
+
+var italic fyne.Resource
+
+var monospace fyne.Resource
+
+var symbol fyne.Resource
